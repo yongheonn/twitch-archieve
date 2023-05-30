d47fa6f5cc2a8982a52f5c2780520c8ef53b16f2
yongheonn <tiger9966@naver.com>
2023-05-31 08:10:39 +0900

    test

--- index.js
+++ index.js
@@ -931,7 +931,7 @@ app.get("/", function (req, res) {
 });
 app.post("/except_games", function (req, res) {
     try {
-        const data = JSON.parse(req.body);
+        const data = req.body.json();
         winston_1.default.info("except_games req body: " + data);
         exceptGames = data.split(",");
         winston_1.default.info("update exceptGames: " + exceptGames);
--- index.ts
+++ index.ts
@@ -1135,7 +1135,7 @@ app.get("/", function (req, res) {
 
 app.post("/except_games", function (req, res) {
   try {
-    const data: string = JSON.parse(req.body);
+    const data: string = req.body.json();
     logger.info("except_games req body: " + data);
     exceptGames = data.split(",");
     logger.info("update exceptGames: " + exceptGames);
